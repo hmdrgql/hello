d8b4757265de82bdfa8673deaf1c96ba0a831e40
yxssss <837828979@qq.com>
2015-11-09 09:57:31 +0800

    1.2

--- books/models.py
+++ books/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Author(models.Model):
-    authorid = models.IntegerField(max_length=30)
+    authorid = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30)
     country = models.CharField(max_length=60)
     age=models.IntegerField(max_length=30)
@@ -15,6 +15,6 @@ class Book(models.Model):
     publisher = models.CharField(max_length=100)
     publication_date = models.DateField(max_length=30)
     price=models.IntegerField(max_length=30)
-    isbn=models.CharField(max_length=30)
+    isbn=models.CharField(primary_key=True)
     def __unicode__(self):
         return self.title
